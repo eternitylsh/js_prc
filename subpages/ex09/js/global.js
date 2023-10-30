784868acbdfd3741364b22df5e7202ff55670c1c
eternitylsh <eternialsh32@naver.com>
2023-10-30 17:48:01 +0900

    오늘한거 커밋

---
+++ subpages/ex09/js/global.js
@@ -0,0 +1,20 @@
+
+
+const strout = {
+    html: instr => {
+        return `${instr}<br>`
+    },
+    clogln: instr => {
+        return `${instr}\n`
+    }
+}
+
+const Gen_rdNumber = (max, min) => {
+    let array = new Uint8Array(2);
+    crypto.getRandomValues(array);
+
+    // 소금치는 처리. 의미는 없어도..
+    return ( ( array[1] + (array[0] * 3)) % max ) + min
+}
+
+const ArrRandrange = (len, min, max) => Array(len).fill().map( x => x = Gen_rdNumber(300, 1) )
