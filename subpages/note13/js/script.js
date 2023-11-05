f3e8b61b0c7e78194ec6a87b3cf0c63e5568d81b
eternitylsh <eternialsh32@naver.com>
2023-11-05 17:36:06 +0900

    시험 체크

--- subpages/note13/js/script.js
+++ subpages/note13/js/script.js
@@ -1,57 +1,76 @@
-const todoBox = document.querySelector('section input')
-const todo = document.querySelector('#todo')
-const add_btn = document.querySelector('section button')
-const list = document.querySelector('#list')
+const todoBox = document.querySelector("section input");
+const todo = document.querySelector("#todo");
+const add_btn = document.querySelector("section button");
+const list = document.querySelector("#list");
 
-let id_count = 0
+let id_count = 0;
+const id_name = "todo-id";
 
 // 추가기능
 const AddTodo = () => {
+  if (0 < todoBox.value.length) {
+    // id 부여
+    const key = ++id_count;
 
-    if( 0 < todoBox.value.length ) {
-        // id 부여
-        const key = ++id_count
+    // 요소 추가 및 설정
+    const item = document.createElement("div");
+    item.setAttribute(id_name, key);
 
-            // 요소 추가 및 설정
-        const item = document.createElement('div')
-        item.setAttribute( 'todo-id', key )
+    const cb = document.createElement("input");
+    cb.type = "checkbox";
 
-        const cb = document.createElement('input')
-        cb.type = 'checkbox'
+    const text = document.createElement("span");
+    text.textContent = todoBox.value;
+    const del = document.createElement("button");
+    del.classList.add("btn", "btn-secondary");
+    del.textContent = "제거";
 
-        const text = document.createElement('span')
-        text.textContent = todoBox.value
-        const del = document.createElement('button')
-        del.textContent = "제거"
+    list.appendChild(item).append(cb, text, del);
 
-        list.appendChild(item).append( cb, text, del )
+    // 이벤트 추가
+    cb.addEventListener("change", (e) => {
+      checkedTodo(e);
+      // item.style.textDecoration = e.target.checked ? "line-through" : "";
+    });
 
-        // 이벤트 추가
-        cb.addEventListener( 'change', e => {
-            item.style.textDecoration = e.target.checked ? 'line-through' : ''
-        } )
+    const checkedTodo = (e) => {
+      if (e.target.checked) {
+        item.style.textDecoration = "line-through";
 
-        del.addEventListener( 'click', () => {
-            removeitem(key)
-        } )
+        const complete = document.createElement("span");
 
-        const removeitem = key => {
-            const t_item = list.querySelector(`[todo-id="${key}"]`)
-            list.removeChild(t_item)
-        }
+        complete.setAttribute(id_name, item.getAttribute(id_name));
+        complete.setAttribute("class", "badge text-bg-success");
+        complete.textContent = "완료";
 
-        // 추가 마무리.
-        todoBox.value = ''
-    }
-    
-}
+        item.appendChild(complete);
+      } else {
+        item.style.textDecoration = "";
 
-document.addEventListener('DOMContentLoaded', () => {
+        const complete = list.querySelector(`span[${id_name}="${key}"]`);
+        item.removeChild(complete);
+      }
+    };
 
-    // 입력시 생성 이벤트 등록
-    add_btn.addEventListener('click', () => AddTodo())
+    del.addEventListener("click", () => {
+      removeitem(key);
+    });
 
-    todoBox.addEventListener( 'keyup', e => {
-        if( 'Enter' === e.key ) AddTodo()
-    } )
-})
+    const removeitem = (key) => {
+      const t_item = list.querySelector(`[todo-id="${key}"]`);
+      list.removeChild(t_item);
+    };
+
+    // 추가 마무리.
+    todoBox.value = "";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  // 입력시 생성 이벤트 등록
+  add_btn.addEventListener("click", () => AddTodo());
+
+  todoBox.addEventListener("keyup", (e) => {
+    if ("Enter" === e.key) AddTodo();
+  });
+});
