13c55a119e88e5802b55c300a4631bf7bb4a0a2d
eternitylsh <eternialsh32@naver.com>
2023-11-13 17:48:16 +0900

    실린더 반복 실행등 커밋;;

--- subpages/ex_nlist/ex_n03/js/cylinder.js
+++ subpages/ex_nlist/ex_n03/js/cylinder.js
@@ -3,16 +3,10 @@ const op_html = document.querySelector('#ex')
 const cylinder = {
     active_obj: op_html.querySelector('#sbox1'),
     ani: {
-        tumbling:  {
-            front: [
-                { transform: 'translateX(0rem)' },
-                { transform: 'translateX(15rem)' },
-            ],
-            back: [
-                { transform: 'translateX(15rem)' },
-                { transform: 'translateX(0rem)' },
-            ]
-        },
+        tumbling:  [
+            { transform: 'translateX(0rem)' },
+            { transform: 'translateX(15rem)' },
+        ],
         timing: {
             duration: 1000,
             iterations: 1,
@@ -23,7 +17,23 @@ const cylinder = {
             after: 0,
         },
     },
+    loop: {
+        id: null,
+        repeat: 0,
+        n_count: 0,
+    },
+    front_d_ids: [null, null],
     play_obj: null,
+    cylinder() {
+        this.OnInit()
+    },
+    OnInit() {
+        this.loop.n_count = 0
+        this.loop.repeat = 0
+        this.loop.id = null
+        this.front_d_ids = this.front_d_ids.fill(null)
+        this.play_obj = null
+    },
     // set 순서 : delay(front, after), duration(play time), 등등..
     // 여기서 단일변수의 경우 공통으로 적용.
     OnSetConfig( __d, __ptime = 1000) {
@@ -42,29 +52,76 @@ const cylinder = {
         cylinder.ani.timing.duration = __ptime
     },
     OnPlay() {
-        window.setTimeout( () => {
-            this.play_obj = this.active_obj.animate(this.ani.tumbling.front, this.ani.timing)
+        this.front_d_ids[0] = window.setTimeout( () => {
+            this.ani.timing.fill = "forwards"
+            this.play_obj = this.active_obj.animate(this.ani.tumbling, this.ani.timing)
             this.play_obj.play()
-            window.setTimeout( () => {
-                this.play_obj = this.active_obj.animate(this.ani.tumbling.back, this.ani.timing)
-                this.play_obj.play()
-            }, Number(this.ani.delay.after) + Number(this.ani.timing.duration) )
+
+            this.play_obj.addEventListener('finish', () => {
+                // 0 : front settime id..
+                if( null !== this.front_d_ids[0] ) {
+                    this.front_d_ids[1] = window.setTimeout( () => {
+                        this.ani.timing.fill = "backwards"
+                        this.play_obj = this.active_obj.animate(this.ani.tumbling, this.ani.timing)
+                        this.play_obj.reverse()
+                    }, Number(this.ani.delay.after) )
+                }
+            })
         }, Number(this.ani.delay.front) ) 
     },
     OnCon_Play(repeat) { // -1 이면 무한.
+        if( null !== this.loop.id ) {
+            console.warn( '정지 후 실행.', this.loop.id )
+            return -1
+        }
+            
         if( 0 < repeat ) {
-            cylinder.ani.timing.iterations = repeat
+            console.log( this.ani.delay.front + this.ani.delay.after + this.ani.timing.duration )
+            this.loop.id = window.setInterval( () => {
+                this.loop.n_count++;
+                this.OnPlay()
+                
+
+                console.log( this.loop.n_count, repeat, repeat <= this.loop.n_count )
+                if( repeat <= this.loop.n_count ) {
+                    window.clearInterval( this.loop.id )
+                    this.loop.id = null
+                    this.loop.n_count = 0
+                    
+                    this.OnStop()
+                }
+            }, Number(this.ani.delay.front) + Number(this.ani.delay.after) + Number(this.ani.timing.duration) * 2 ) 
         } else if ( -1 === repeat ) {
-            cylinder.ani.timing.iterations = Infinity
+            console.log("무한루프..")
+            this.loop.id = window.setInterval( () => {
+                this.OnPlay()
+            }, Number(this.ani.delay.front) + Number(this.ani.delay.after) + Number(this.ani.timing.duration) * 2 ) 
         } else {
             console.error( 'wrong input repeat', repeat )
-            return -1;
+            return -1
         }
 
         this.OnPlay()
     },
     OnStop() {
         this.play_obj.cancel()
+
+        this.front_d_ids.forEach( (e, idx) => {
+            if( null !== e )
+                window.clearTimeout(e)
+            this.front_d_ids[idx] = null
+        } )
+
+        if( null !== this.loop.id ) {
+            window.clearInterval( this.loop.id )
+            this.loop.id = null
+        }
+        
+        this.OnAniInit()
+    },
+    OnAniInit() {
+        this.play_obj = this.active_obj.animate(this.ani.tumbling, this.ani.timing)
+        this.play_obj.pause()
     }
 }
 
